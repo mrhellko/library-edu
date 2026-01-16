77b0061375489dfd8f7dd8ec8c90b0e997ee5e63
Overbyaka <malenkayaelfiyka@yandex.ru>
2026-01-16 16:39:37 +0300

    done task-2

--- src/main/resources/db/changelog/20260114-books.sql
+++ src/main/resources/db/changelog/20260114-books.sql
@@ -3,8 +3,9 @@
 --changeset RA:1
 CREATE TABLE books (
     id bigint PRIMARY KEY,
-    book_name text
+    book_name text,
+    author_name text
 );
 --changeset RA:2
-INSERT INTO books (id, book_name) VALUES (1, 'Гарри Поттер');
-INSERT INTO books (id, book_name) VALUES (2, 'Задача трех тел');
+INSERT INTO books (id, book_name, author_name) VALUES (1, 'Гарри Поттер', 'Джоан Роулинг');
+INSERT INTO books (id, book_name, author_name) VALUES (2, 'Задача трех тел', 'Лю Цысинь');
