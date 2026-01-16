6ff881a207e73d4beed3784b8954d50318d2568a
Overbyaka <malenkayaelfiyka@yandex.ru>
2026-01-16 20:24:29 +0300

    TASK-4: Рефакторинг кода, замена дублирования кода на переменную

--- src/main/java/ru/mrhellko/library/dao/BookDAO.java
+++ src/main/java/ru/mrhellko/library/dao/BookDAO.java
@@ -2,6 +2,7 @@ package ru.mrhellko.library.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Service;
 import ru.mrhellko.library.Entity.Book;
 
@@ -15,28 +16,23 @@ public class BookDAO {
     private static final String GET_ALL_BOOK = "select * from books";
     @Autowired
     private JdbcTemplate jdbcTemplate;
+    private final RowMapper<Book> bookRowMapper = (resultSet, rowNum) -> {
+        final Book book = new Book();
+        book.setId(resultSet.getLong("id"));
+        book.setBookName(resultSet.getString("book_name"));
+        book.setAuthor(resultSet.getString("author_name"));
+        return book;
+    };
 
     public Book getBookById(long id) {
         return jdbcTemplate.queryForObject(
                 GET_BOOK_BY_ID_SQL,
-                (resultSet, rowNum) -> {
-                    final Book book = new Book();
-                    book.setId(resultSet.getLong("id"));
-                    book.setBookName(resultSet.getString("book_name"));
-                    book.setAuthor(resultSet.getString("author_name"));
-                    return book;
-                },
+                bookRowMapper,
                 id
         );
     }
 
     public List<Book> getAll(){
-        return jdbcTemplate.query(GET_ALL_BOOK, (resultSet, rowNum) -> {
-            final Book book = new Book();
-            book.setId(resultSet.getLong("id"));
-            book.setBookName(resultSet.getString("book_name"));
-            book.setAuthor(resultSet.getString("author_name"));
-            return book;
-        });
+        return jdbcTemplate.query(GET_ALL_BOOK, bookRowMapper);
     }
 }
